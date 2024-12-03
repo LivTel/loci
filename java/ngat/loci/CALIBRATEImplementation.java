ed656f3cbdc73b8dbb13dcd0b0cdd60287c80a89
Chris Mottram <C.Mottram@ljmu.ac.uk>
2024-12-03 10:41:33 +0000

    Completed sendTakeBiasFrameCommand implementation in CALIBRATEImplementation.java.

--- java/ngat/loci/CALIBRATEImplementation.java
+++ java/ngat/loci/CALIBRATEImplementation.java
@@ -3,6 +3,7 @@
 package ngat.loci;
 
 import java.lang.*;
+import java.net.*;
 import java.text.*;
 import java.util.*;
 
@@ -16,7 +17,7 @@ import ngat.util.logging.*;
  * This class provides common methods to send bias and dark commands to the ccd-ctrl CCD Flask API,
  * as need by several Bias and Dark calibration implementations.
  * @version $Revision: CALIBRATEImplementation.java $
- * @see ngat.liric.HardwareImplementation
+ * @see ngat.loci.HardwareImplementation
  */
 public class CALIBRATEImplementation extends HardwareImplementation implements JMSCommandImplementation
 {
@@ -62,15 +63,68 @@ public class CALIBRATEImplementation extends HardwareImplementation implements J
 
 	/**
 	 * Send a 'takeBiasFrame' command to the loci-ctrl CCD Flask API.
+	 * <ul>
+	 * <li>We call getCCDFlaskConnectionData to setup ccdFlaskHostname and ccdFlaskPortNumber.
+	 * <li>We generate a new FITS filename to store the Bias data into.
+	 * <li>We setup and configure an instance of TakeBiasFrameCommand, 
+	 *     with connection details and the generated filename.
+	 * <li>We run the instance of TakeBiasFrameCommand.
+	 * <li>We check whether a run exception occured, and throw it as an exception if so.
+	 * <li>We log the return status and message.
+	 * <li>We check whether the TakeBiasFrameCommand return status was Success, and throw an exception if it
+	 *     returned a failure.
+	 * <li>We return the generated bias filename.
+	 * </ul>
+	 * @return The generated BIAS FITS filename is returned as a String.
+	 * @see #getCCDFlaskConnectionData
+	 * @see #ccdFlaskHostname
+	 * @see #ccdFlaskPortNumber
+	 * @see ngat.loci.ccd.TakeBiasFrameCommand
+	 * @exception UnknownHostException Thrown if the address passed to TakeBiasFrameCommand.setAddress is not a 
+	 *            valid host.
+	 * @exception Exception Thrown if the TakeBiasFrameCommand generates a run exception, or the return
+	 *            status is not success.
 	 */
-	protected void sendTakeBiasFrameCommand()
+	protected String sendTakeBiasFrameCommand() throws UnknownHostException, Exception
 	{
 		TakeBiasFrameCommand takeBiasFrameCommand = null;
-
-		loci.log(Logging.VERBOSITY_INTERMEDIATE,"sendTakeBiasFrameCommand:started.");
-		takeBiasFrameCommand = new TakeBiasFrameCommand();
-		diddly
-		takeBiasFrameCommand.setAddress(
-		loci.log(Logging.VERBOSITY_INTERMEDIATE,"sendTakeBiasFrameCommand:finished.");
+		String filename = null;
 		
+		loci.log(Logging.VERBOSITY_INTERMEDIATE,"sendTakeBiasFrameCommand:started.");
+		// get CCD Flask API connection data
+		getCCDFlaskConnectionData();
+		// setup filename to save bias frame into
+		lociFitsFilename.nextRunNumber();
+		filename = lociFitsFilename.getFilename();
+		loci.log(Logging.VERBOSITY_INTERMEDIATE,
+			 "sendTakeBiasFrameCommand:Saving Bias frame to filename:"+filename);
+		// setup TakeBiasFrameCommand
+		takeBiasFrameCommand = new TakeBiasFrameCommand();
+		takeBiasFrameCommand.setAddress(ccdFlaskHostname);
+		takeBiasFrameCommand.setPortNumber(ccdFlaskPortNumber);
+		takeBiasFrameCommand.setFilename(filename);
+		// run command
+		takeBiasFrameCommand.run();
+		// check reply
+		if(takeBiasFrameCommand.getRunException() != null)
+		{
+			throw new Exception(this.getClass().getName()+
+					    ":sendTakeBiasFrameCommand:Failed to take bias frame:",
+					    takeBiasFrameCommand.getRunException());
+		}
+		loci.log(Logging.VERBOSITY_VERBOSE,
+			 "sendTakeBiasFrameCommand:Take Bias Frame Command Finished with status: "+
+			 takeBiasFrameCommand.getReturnStatus()+
+			 " and message:"+takeBiasFrameCommand.getMessage()+".");
+		if(takeBiasFrameCommand.isReturnStatusSuccess() == false)
+		{
+			throw new Exception(this.getClass().getName()+
+					    ":sendTakeBiasFrameCommand:Take Bias Frame Command failed with status: "+
+					    takeBiasFrameCommand.getReturnStatus()+
+					    " and message:"+takeBiasFrameCommand.getMessage()+".");
+		}
+		loci.log(Logging.VERBOSITY_INTERMEDIATE,"sendTakeBiasFrameCommand:finished with filename:"+filename);
+		return filename;
 	}
+}
+
